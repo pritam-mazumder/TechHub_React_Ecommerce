41ed7ff17cb1f29a5ed41dca6f6ada550472b4e7
Pritam <itspritammazumder@gmail.com>
2023-03-31 07:39:59 +0530

    31-03-2023: Login and Signup page added and Database created

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
+import Helmet from "../components/Helmet/Helmet";
+import { Container, Row, Col, Form, FormGroup } from "reactstrap";
+import { Link } from "react-router-dom";
+
+import "../styles/login.css";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>Login</div>
-  )
-}
+    <Helmet title="Login">
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" className="m-auto text-center p-5">
+              <h3 className="fw-bold mb-4">Login</h3>
 
-export default Login
+              <Form className="auth__form">
+                <FormGroup className="form__group">
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FormGroup>
 
-//9.48
+                <button type="submit" className="buy__btn auth__btn w-100">
+                  Login
+                </button>
+                <p className="mt-4">
+                  Don't have an account?{" "}
+                  <Link to="/signup"> Create an account</Link>
+                </p>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Login;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,113 @@
-import React from 'react'
+import React, { useState } from "react";
+import Helmet from "../components/Helmet/Helmet";
+import { Container, Row, Col, Form, FormGroup } from "reactstrap";
+import { Link } from "react-router-dom";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
+import { auth } from "../firebase.config";
+import { storage } from "../firebase.config";
+import { setDoc, doc } from "firebase/firestore";
+import { db } from "../firebase.config";
+
+import { toast } from "react-toastify";
+
+import "../styles/login.css";
+import { async } from "@firebase/util";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [file, setFile] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-export default Signup
+  const signup = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const user = userCredential.user;
+
+      const storageRef = ref(storage, `images/${Date.now() + username}`)
+      const uploadTask = uploadBytesResumable(storageRef, file)
+
+      uploadTask.on((error)=>{
+        toast.error(error.message)
+      }, ()=>{
+        getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL)=>{
+          updateProfile(user,{
+            displayName: username,
+            photoURL: downloadURL
+          })
+        })
+      })
+
+      console.log(user);
+    } catch (error) {
+      toast.error("something went wrong");
+    }
+  };
+
+  return (
+    <Helmet title="Signup">
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" className="m-auto text-center p-5">
+              <h3 className="fw-bold mb-4">Signup</h3>
+
+              <Form className="auth__form" onSubmit={signup}>
+                <FormGroup className="form__group">
+                  <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input
+                    type="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </FormGroup>
+                <FormGroup className="form__group">
+                  <input
+                    type="file"
+                    onChange={(e) => setFile(e.target.files[0])}
+                  />
+                </FormGroup>
+
+                <button type="submit" className="buy__btn auth__btn w-100">
+                  Create an account
+                </button>
+                <p className="mt-4">
+                  Already have an account? <Link to="/login"> Login</Link>
+                </p>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Signup;
